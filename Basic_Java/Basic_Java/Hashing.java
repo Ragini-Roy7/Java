7f4973538f505f4969f4c905b780c11a6d8a5900
Ragini-Roy7 <royragini967@gmail.com>
2026-02-23 12:57:53 +0530

    Find Duplicate Optimal Sol

--- Basic_Java/Basic_Java/Hashing.java
+++ Basic_Java/Basic_Java/Hashing.java
@@ -1,5 +1,6 @@
 package Basic_Java;
 import java.util.HashSet;
+import java.util.Iterator;
 
 public class Hashing {
     public static void main(String[] args){
@@ -8,11 +9,30 @@ public class Hashing {
         set.add(10);
         set.add(90);
         set.add(89);
+        set.add(89);
         System.out.println(set);
 
         //remove function
         set.remove(89);
         System.out.println("89 removed");
+        System.out.println(set);
+//iterator
+        Iterator it= set.iterator();
+        System.out.println(it);
+        //set.iterator() returns iterator object
+        //traverse elements of HashSet
+        //prints default toString() representation of obj
+        //Since Iterator doesn’t override toString(), you just see the class name (java.util.HashMap$KeyIterator) plus its memory reference hash.
+
+//print its elements
+        Iterator <Integer> i= set.iterator();
+        while(i.hasNext()) {
+            System.out.println(i.next());
+        }
+        Iterator <Integer> r= set.iterator();
+        while(r.hasNext()) {
+            System.out.println(r.next());
+        }
 
     }
 }
