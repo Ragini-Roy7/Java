517f08f3eed36b671b1891e35fe7d2e1ef503981
Ragini-Roy7 <royragini967@gmail.com>
2026-02-24 20:29:47 +0530

    Reverse Array

---
+++ Basic_Java/Arrays/Pairs_In_Array.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+public class Pairs_In_Array {
+    public static void print_pairs(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            //print current elements
+            int current_ele = arr[i];
+
+            for (int j = i + 1; j < arr.length; j++) {
+                System.out.println("(" + arr[i] + "," + arr[j] + ")");
+
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 6, 8, 10, 12, 14, 16};
+        print_pairs(arr);
+    }
+}
