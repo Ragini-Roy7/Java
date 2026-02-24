98548d1db7944db3a8a09dfb7c998cbf73779403
Ragini-Roy7 <royragini967@gmail.com>
2026-02-24 23:00:05 +0530

    Array Basic

--- Basic_Java/Arrays/Pairs_In_Array.java
+++ Basic_Java/Arrays/Pairs_In_Array.java
@@ -2,20 +2,30 @@ package Arrays;
 
 public class Pairs_In_Array {
     public static void print_pairs(int[] arr) {
+        int total_pairs = 0;
         for (int i = 0; i < arr.length; i++) {
             //print current elements
+            //total possible pairs
+
             int current_ele = arr[i];
 
             for (int j = i + 1; j < arr.length; j++) {
-                System.out.println("(" + arr[i] + "," + arr[j] + ")");
+                System.out.print("(" + arr[i] + "," + arr[j] + ")");
+                total_pairs++;
 
             }
+            System.out.println();
+//            System.out.println("Total Possible Pairs:"+total_pairs);
+
         }
+
+        System.out.println("Total Possible Pairs:" + total_pairs);
 
     }
 
     public static void main(String[] args) {
         int[] arr = {2, 4, 6, 8, 10, 12, 14, 16};
         print_pairs(arr);
+
     }
 }
