d4fd2735853563bb5b7dc88984b3a9e551389d8d
Ragini-Roy7 <raginicodes@gmail.com>
2026-04-01 22:47:44 +0530

    Sum of Matrix Elements

---
+++ Basic_Java/N_D_Array/Sum_Elements.java
@@ -0,0 +1,26 @@
+package N_D_Array;
+import java.util.*;
+
+public class Sum_Elements {
+    public static void main(String[] args) {
+        System.out.println("Enter no of rows");
+        Scanner sc= new Scanner(System.in);
+        int rows=sc.nextInt();
+
+        System.out.println("Enter no of cols");
+        int cols= sc.nextInt();
+        int[][] matrix=new int [rows][cols];
+        int sum=0;
+
+        //ask user to enter elements
+        System.out.println("Enter elements within matrix");
+        for(int i=0;i<rows;i++) {
+            for(int j=0;j<cols;j++) {
+                matrix[i][j]=sc.nextInt();
+                sum= sum+matrix[i][j];
+
+            }
+        }
+        System.out.println("Sum is : "+sum);
+    }
+}
