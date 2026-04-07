ea0a1ca9f40a23ffef69cd2e2f0b9a9fb145aba9
Ragini-Roy7 <raginicodes@gmail.com>
2026-04-08 00:34:05 +0530

    HashSet Intro

--- Basic_Java/Collections/HashSet/HashSet_Intro.java
+++ Basic_Java/Collections/HashSet/HashSet_Intro.java
@@ -1,5 +1,8 @@
 package Collections.HashSet;
 import java.util.*;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.lang.Integer;
 
 public class HashSet_Intro {
     public static void main(String[] args) {
@@ -11,8 +14,31 @@ public class HashSet_Intro {
         //synchronization needed to make it thread safe
         hs.add(456);
         hs.add(678);
-        hs.add(456);
+        hs.add(454);
         System.out.println("size of hash set is: "+hs.size());
         System.out.println("elements in HashSet: "+hs);
+
+        //removing elements in hashset
+        hs.remove(456);
+        System.out.println("Removed element from hashset is : "+hs);
+
+        //iterating through hashset
+        Iterator<Integer> iterator= hs.iterator();
+        while(iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+        System.out.println(hs.isEmpty()); //false
+        System.out.println(hs.clone());
+        System.out.println(hs.contains(678 ));
+        System.out.println(hs.add(788));
+//        while(!iterator.hasNext()){
+//            System.out.println("empty");
+//        }
+        System.out.println();
+        //printing elements via for each
+        for(Integer element : hs){
+            System.out.println(element);
+        }
+
     }
 }
