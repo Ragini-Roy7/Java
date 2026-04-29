3bb743cb21839a4b2243fdc677a9d844641e8e79
Ragini-Roy7 <raginicodes@gmail.com>
2026-04-30 01:00:23 +0530

    Basic Recursion

---
+++ Basic_Java/src/Static_Non_Static/Recursion/Fibonacci.java
@@ -0,0 +1,24 @@
+package src.Static_Non_Static.Recursion;
+
+public class Fibonacci {
+    public static int fibo(int n){
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+        //fib->sum of its two prev sum
+        //n-1 +n-2
+        int fib1= fibo(n-1);
+        int fib2= fibo(n-2);
+        int fn= fib1+fib2;
+        return fn;
+
+    }
+
+    public static void main(String[] args) {
+        int n=6;
+        System.out.println(fibo(n));
+    }
+}
