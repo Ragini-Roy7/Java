673547d7bf1ab83abc1df42e56d31ab90d7f55b5
Ragini Roy <raginicodes@gmail.com>
2026-06-15 21:19:11 +0530

    max product using kadanes algo LC

---
+++ Basic_Java/Arrays/Arrays/LeetCode/maxProduct.java
@@ -0,0 +1,29 @@
+package Arrays.LeetCode;
+import java.util.*;
+public class maxProduct {
+    //using kadane algo
+    public static int maxProd(int[]nums){
+        int max=nums[0];
+        int min=nums[0];
+        int res= nums[0];
+        for(int i=1;i<nums.length;i++) {
+            //if current element is negative element swap it with max and min
+            if (nums[i] < 0) {
+                int temp = max;
+                max = min;
+                min = temp;
+            }
+            max=Math.max(nums[i], max*nums[i]);
+            min=Math.min(nums[i], min*nums[i]);
+            res= Math.max(res,max);
+        }
+        return res;
+
+    }
+
+    public static void main(String[] args) {
+        int[]nums= {3,4,-1,3};
+        int res=maxProd(nums);
+        System.out.println(res);
+    }
+}
