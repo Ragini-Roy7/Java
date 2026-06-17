faed7f81069b9ac6ad132d7f2b090978eca899cc
Ragini Roy <raginicodes@gmail.com>
2026-06-18 00:01:19 +0530

    min path sum lc- dp approacj

--- Basic_Java/Arrays/Arrays/LeetCode/MinPathSum.java
+++ Basic_Java/Arrays/Arrays/LeetCode/MinPathSum.java
@@ -1,31 +1,36 @@
-package Arrays.LeetCode;
+public class minPathSum {
 
-public class MinPathSum {
+    public static int minPath(int[][] grid) {
 
-        public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
 
-            int m = grid.length;
-            int n = grid[0].length;
-
-            // Fill first column
-            for (int i = 1; i < m; i++) {
-                grid[i][0] += grid[i - 1][0];
-            }
-
-            // Fill first row
-            for (int j = 1; j < n; j++) {
-                grid[0][j] += grid[0][j - 1];
-            }
-
-            // Fill remaining cells
-            for (int i = 1; i < m; i++) {
-                for (int j = 1; j < n; j++) {
-                    grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
-                }
-            }
-
-            return grid[m - 1][n - 1];
+        // Fill first column
+        for (int i = 1; i < m; i++) {
+            grid[i][0] += grid[i - 1][0];
         }
+
+        // Fill first row
+        for (int j = 1; j < n; j++) {
+            grid[0][j] += grid[0][j - 1];
+        }
+
+        // Fill remaining cells
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
+            }
+        }
+
+        return grid[m - 1][n - 1];
+    }
+}
+public static void main(String[]args){
+            int[][]grid= {{1,2,1},
+                    {1,3,1},
+                    {2,3,1}};
+            int res= minPathSum.minPath(grid);
+    System.out.println(res);
 
     }
 
