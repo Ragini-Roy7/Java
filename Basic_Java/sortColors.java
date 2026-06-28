6903c0396f67415a84d81bd8f770a0f94e1a14d9
Ragini Roy <raginicodes@gmail.com>
2026-06-28 23:51:22 +0530

    sort colors lc

---
+++ Basic_Java/sortColors.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class sortColors {
+
+    // Dutch National Flag Algorithm
+    public static void sortColors(int[] nums) {
+        int low = 0, mid = 0, high = nums.length - 1;
+
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums, low, mid);
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else { // nums[mid] == 2
+                swap(nums, mid, high);
+                high--;
+            }
+        }
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0, 2, 1, 0, 0};
+
+        sortColors(nums); // no return value
+
+        System.out.println(Arrays.toString(nums));
+    }
+}
