0dac7c282e25a1bee5430aef8b8738cdc9fe230a
Ragini Roy <raginicodes@gmail.com>
2026-07-07 23:55:07 +0530

    group anagrams- hashmap based appraoch -lc

---
+++ Basic_Java/Arrays/Arrays/LeetCode/sorting/groupAnagrams.java
@@ -0,0 +1,25 @@
+package Arrays.LeetCode.sorting;
+import java.util.*;
+public class groupAnagrams {
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String word : strs) {
+
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+
+            String key = new String(chars);
+
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(word);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+
+    public static void main(String[] args) {
+        String[] strs= {"tan", "hat", "cat", "map", "rat", "sat"};
+        System.out.println(groupAnagrams(strs));
+    }
+}
