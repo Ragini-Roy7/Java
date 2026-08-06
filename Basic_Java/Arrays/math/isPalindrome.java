fe7d5b3bd3e0265f1b6ddf938570bbeee197239e
Ragini Roy <raginicodes@gmail.com>
2026-08-06 23:59:15 +0530

    basic math in dsa

---
+++ Basic_Java/Arrays/math/isPalindrome.java
@@ -0,0 +1,26 @@
+package math;
+
+public class isPalindrome {
+    public static  boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        int original = x;
+        int reverse = 0;
+
+        while (x > 0) {
+            int digit = x % 10;
+            reverse = reverse * 10 + digit;
+            x = x / 10;
+        }
+
+        return original == reverse;
+    }
+
+    public static void main(String[] args) {
+        int x=121;
+        System.out.println(isPalindrome(x));
+
+    }
+}
