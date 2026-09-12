796bff69bab6a3f98823630ea079bf885fff99c0
Ragini Roy <raginicodes@gmail.com>
2026-09-12 23:39:21 +0530

    missing num-lc

---
+++ Basic_Java/Arrays/Arrays/LeetCode/missingNum.java
@@ -0,0 +1,22 @@
+package Arrays.LeetCode;
+import java.util.*;
+public class missingNum {
+    public  static int missingNumber(int[] nums) {
+        int n = nums.length;
+
+        int expectedSum = n * (n + 1) / 2;
+
+        int actualSum = 0;
+
+        for (int num : nums) {
+            actualSum += num;
+        }
+
+        return expectedSum - actualSum;
+    }
+
+    public static void main(String[] args) {
+        int[] nums={1,0,3};
+        System.out.println( missingNum(nums));
+    }
+}
